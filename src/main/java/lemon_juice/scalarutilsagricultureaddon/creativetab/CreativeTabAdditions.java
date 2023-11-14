935c774ea7c7402e8e2b0e14e5be561d8fe80e1c
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-11-14 01:07:21 -0500

    5.3.14 - Removed Creative Tab, now Creative Tab is through Scalar Utils base mod.

--- src/main/java/lemon_juice/scalarutilsagricultureaddon/creativetab/CreativeTabAdditions.java
+++ src/main/java/lemon_juice/scalarutilsagricultureaddon/creativetab/CreativeTabAdditions.java
@@ -1,34 +1,26 @@
 package lemon_juice.scalarutilsagricultureaddon.creativetab;
 
+import lemon_juice.scalar_utils.creativetab.ModCreativeTab;
 import lemon_juice.scalarutilsagricultureaddon.ScalarUtilsAgricultureAddon;
 import lemon_juice.scalarutilsagricultureaddon.item.ModItems;
 import lemon_juice.scalarutilsagricultureaddon.item.seed_items.AbstractBaseSeedItem;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
-public class ModCreativeTab {
+public class CreativeTabAdditions {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ScalarUtilsAgricultureAddon.MOD_ID);
 
-    public static final RegistryObject<CreativeModeTab> SCALAR_UTILS_AGRICULTURE_ADDON_TAB = CREATIVE_MODE_TABS.register("scalar_utils_agriculture_addon", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.scalar_utils_agriculture_addon"))
-            .icon(() -> new ItemStack(ModItems.NETHERITE_SEEDS.get()))
-            .build());
-
     public static void registerTabs(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTab() == SCALAR_UTILS_AGRICULTURE_ADDON_TAB.get()) {
-
+        if (event.getTab() == ModCreativeTab.SCALAR_UTILS_AGRICULTURE_TAB.get()) {
             for (int i = 1; i <= 5; i++) {
                 for(RegistryObject<Item> item : ModItems.ITEMS.getEntries())
                     if(item.get() instanceof AbstractBaseSeedItem abstractBaseSeedItem && abstractBaseSeedItem.getTier() == i) event.accept(abstractBaseSeedItem);
             }
-
         }
     }
 
